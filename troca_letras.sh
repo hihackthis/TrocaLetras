3db70d2444a33658f7f920d537ad7c7d34906ae3
hihackthis <hihackthis@gmail.com>
2023-10-26 14:02:14 -0300

    Update tool

--- troca_letras.sh
+++ troca_letras.sh
@@ -24,9 +24,9 @@ ENDCOLOR="\e[0m"
 echo "Vamos começar, antes de entrar no programa..."
 sleep 2
 echo ""
-read -p "Digite o caminho absoluto do diretório: " DIR
+read -p "Digite o caminho absoluto do diretório: " dir
 echo ""
-echo -e "Você digitou: ${MAGENTA_LIGHT}${DIR}${ENDCOLOR}"
+echo -e "Você digitou: ${MAGENTA_LIGHT}${dir}${ENDCOLOR}"
 echo ""
 
 # Função de busca e troca as letras
@@ -55,19 +55,19 @@ echo ""
 			echo ""
 			echo -e "${YELLOW_LIGHT}Trocando à por a:${ENDCOLOR}"
 			echo ""
-			find "${DIR}" -depth -execdir rename -v -n "s/à/a/g" {} \;
+			find "${dir}" -depth -exec rename -v -n "s/à/a/g" {} \;
 			echo ""
 			echo -e "${YELLOW_LIGHT}Trocando á por a:${ENDCOLOR}"
 			echo ""
-			find "${DIR}" -depth -execdir rename -v -n "s/á/a/g" {} \;
+			find "${dir}" -depth -exec rename -v -n "s/á/a/g" {} \;
 			echo ""
 			echo -e "${YELLOW_LIGHT}Trocando ã por a:${ENDCOLOR}"
 			echo ""
-			find "${DIR}" -depth -execdir rename -v -n "s/ã/a/g" {} \;
+			find "${dir}" -depth -exec rename -v -n "s/ã/a/g" {} \;
 			echo ""
 			echo -e "${YELLOW_LIGHT}Trocando â por a:${ENDCOLOR}"
 			echo ""
-			find "${DIR}" -depth -execdir rename -v -n "s/â/a/g" {} \;
+			find "${dir}" -depth -exec rename -v -n "s/â/a/g" {} \;
 			echo ""
 			echo -e "${GRAY_LIGHT}Fim do teste!${ENDCOLOR}"
 			echo ""
@@ -78,55 +78,55 @@ echo ""
 			echo ""
 			echo -e "${YELLOW_LIGHT}Trocando à por a:${ENDCOLOR}"
 			echo ""
-			find "${DIR}" -depth -execdir rename -v "s/à/a/g" {} \;
+			find "${dir}" -depth -exec rename -v "s/à/a/g" {} \;
 			echo ""
 			echo -e "${YELLOW_LIGHT}Trocando á por a:${ENDCOLOR}"
 			echo ""
-			find "${DIR}" -depth -execdir rename -v "s/á/a/g" {} \;
+			find "${dir}" -depth -exec rename -v "s/á/a/g" {} \;
 			echo ""
 			echo -e "${YELLOW_LIGHT}Trocando ã por a:${ENDCOLOR}"
 			echo ""
-			find "${DIR}" -depth -execdir rename -v "s/ã/a/g" {} \;
+			find "${dir}" -depth -exec rename -v "s/ã/a/g" {} \;
 			echo ""
 			echo -e "${YELLOW_LIGHT}Trocando â por a:${ENDCOLOR}"
 			echo ""
-			find "${DIR}" -depth -execdir rename -v "s/â/a/g" {} \;
+			find "${dir}" -depth -exec rename -v "s/â/a/g" {} \;
 			echo ""
 			echo -e "${YELLOW_LIGHT}Trocando é por e:${ENDCOLOR}"
 			echo ""
-			find "${DIR}" -depth -execdir rename -v "s/é/e/g" {} \;
+			find "${dir}" -depth -exec rename -v "s/é/e/g" {} \;
 			echo ""
 			echo -e "${YELLOW_LIGHT}Trocando ê por e:${ENDCOLOR}"
 			echo ""
-			find "${DIR}" -depth -execdir rename -v "s/ê/e/g" {} \;
+			find "${dir}" -depth -exec rename -v "s/ê/e/g" {} \;
 			echo ""
 			echo -e "${YELLOW_LIGHT}Trocando í por i:${ENDCOLOR}"
 			echo ""
-			find "${DIR}" -depth -execdir rename -v "s/í/i/g" {} \;
+			find "${dir}" -depth -exec rename -v "s/í/i/g" {} \;
 			echo ""
 			echo -e "${YELLOW_LIGHT}Trocando ó por o:${ENDCOLOR}"
 			echo ""
-			find "${DIR}" -depth -execdir rename -v "s/ó/o/g" {} \;
+			find "${dir}" -depth -exec rename -v "s/ó/o/g" {} \;
 			echo ""
 			echo -e "${YELLOW_LIGHT}Trocando õ por o:${ENDCOLOR}"
 			echo ""
-			find "${DIR}" -depth -execdir rename -v "s/õ/o/g" {} \;
+			find "${dir}" -depth -exec rename -v "s/õ/o/g" {} \;
 			echo ""
 			echo -e "${YELLOW_LIGHT}Trocando ô por o:${ENDCOLOR}"
 			echo ""
-			find "${DIR}" -depth -execdir rename -v "s/ô/o/g" {} \;
+			find "${dir}" -depth -exec rename -v "s/ô/o/g" {} \;
 			echo ""
 			echo -e "${YELLOW_LIGHT}Trocando ú por u:${ENDCOLOR}"
 			echo ""
-			find "${DIR}" -depth -execdir rename -v "s/ú/u/g" {} \;
+			find "${dir}" -depth -exec rename -v "s/ú/u/g" {} \;
 			echo ""
 			echo -e "${YELLOW_LIGHT}Trocando ü por u:${ENDCOLOR}"
 			echo ""
-			find "${DIR}" -depth -execdir rename -v "s/ü/u/g" {} \;
+			find "${dir}" -depth -exec rename -v "s/ü/u/g" {} \;
 			echo ""
 			echo -e "${YELLOW_LIGHT}Trocando ç por c:${ENDCOLOR}"
 			echo ""
-			find "${DIR}" -depth -execdir rename -v "s/ç/c/g" {} \;
+			find "${dir}" -depth -exec rename -v "s/ç/c/g" {} \;
 			echo ""
 			echo -e "${GRAY_LIGHT}Fim das substituições!${ENDCOLOR}"
 			echo ""
@@ -137,55 +137,55 @@ echo ""
 			echo ""
 			echo -e "${YELLOW_LIGHT}Trocando À por A:${ENDCOLOR}"
 			echo ""
-			find "${DIR}" -depth -execdir rename -v "s/À/A/g" {} \;
+			find "${dir}" -depth -exec rename -v "s/À/A/g" {} \;
 			echo ""
 			echo -e "${YELLOW_LIGHT}Trocando Á por A:${ENDCOLOR}"
 			echo ""
-			find "${DIR}" -depth -execdir rename -v "s/Á/A/g" {} \;
+			find "${dir}" -depth -exec rename -v "s/Á/A/g" {} \;
 			echo ""
 			echo -e "${YELLOW_LIGHT}Trocando Ã por A:${ENDCOLOR}"
 			echo ""
-			find "${DIR}" -depth -execdir rename -v "s/Ã/A/g" {} \;
+			find "${dir}" -depth -exec rename -v "s/Ã/A/g" {} \;
 			echo ""
 			echo -e "${YELLOW_LIGHT}Trocando Â por A:${ENDCOLOR}"
 			echo ""
-			find "${DIR}" -depth -execdir rename -v "s/Â/A/g" {} \;
+			find "${dir}" -depth -exec rename -v "s/Â/A/g" {} \;
 			echo ""
 			echo -e "${YELLOW_LIGHT}Trocando É por E:${ENDCOLOR}"
 			echo ""
-			find "${DIR}" -depth -execdir rename -v "s/É/E/g" {} \;
+			find "${dir}" -depth -exec rename -v "s/É/E/g" {} \;
 			echo ""
 			echo -e "${YELLOW_LIGHT}Trocando Ê por E:${ENDCOLOR}"
 			echo ""
-			find "${DIR}" -depth -execdir rename -v "s/Ê/E/g" {} \;
+			find "${dir}" -depth -exec rename -v "s/Ê/E/g" {} \;
 			echo ""
 			echo -e "${YELLOW_LIGHT}Trocando Í por I:${ENDCOLOR}"
 			echo ""
-			find "${DIR}" -depth -execdir rename -v "s/Í/I/g" {} \;
+			find "${dir}" -depth -exec rename -v "s/Í/I/g" {} \;
 			echo ""
 			echo -e "${YELLOW_LIGHT}Trocando Ó por O:${ENDCOLOR}"
 			echo ""
-			find "${DIR}" -depth -execdir rename -v "s/Ó/O/g" {} \;
+			find "${dir}" -depth -exec rename -v "s/Ó/O/g" {} \;
 			echo ""
 			echo -e "${YELLOW_LIGHT}Trocando Õ por O:${ENDCOLOR}"
 			echo ""
-			find "${DIR}" -depth -execdir rename -v "s/Õ/O/g" {} \;
+			find "${dir}" -depth -exec rename -v "s/Õ/O/g" {} \;
 			echo ""
 			echo -e "${YELLOW_LIGHT}Trocando Ô por O:${ENDCOLOR}"
 			echo ""
-			find "${DIR}" -depth -execdir rename -v "s/Ô/O/g" {} \;
+			find "${dir}" -depth -exec rename -v "s/Ô/O/g" {} \;
 			echo ""
 			echo -e "${YELLOW_LIGHT}Trocando Ú por U:${ENDCOLOR}"
 			echo ""
-			find "${DIR}" -depth -execdir rename -v "s/Ú/U/g" {} \;
+			find "${dir}" -depth -exec rename -v "s/Ú/U/g" {} \;
 			echo ""
 			echo -e "${YELLOW_LIGHT}Trocando Ü por U:${ENDCOLOR}"
 			echo ""
-			find "${DIR}" -depth -execdir rename -v "s/Ü/U/g" {} \;
+			find "${dir}" -depth -exec rename -v "s/Ü/U/g" {} \;
 			echo ""
 			echo -e "${YELLOW_LIGHT}Trocando Ç por C:${ENDCOLOR}"
 			echo ""
-			find "${DIR}" -depth -execdir rename -v "s/Ç/C/g" {} \;
+			find "${dir}" -depth -exec rename -v "s/Ç/C/g" {} \;
 			echo ""
 			echo -e "${GRAY_LIGHT}Fim das substituições!${ENDCOLOR}"
 			echo ""
@@ -207,11 +207,11 @@ done
 
 # Caso o diretório não exista
 
-if [ -d "${DIR}" ]
+if [ -d "${dir}" ]
 then
-	trocaLetras "${DIR}"
+	trocaLetras "${dir}"
 else
-	echo -e "${RED_LIGHT}Diretório${ENDCOLOR} ${MAGENTA_LIGHT}${DIR}${ENDCOLOR}\
+	echo -e "${RED_LIGHT}Diretório${ENDCOLOR} ${MAGENTA_LIGHT}${dir}${ENDCOLOR}\
 	${RED_LIGHT}não existe, saindo... tchau!${ENDCOLOR}"
 	sleep 2
 	exit 1
